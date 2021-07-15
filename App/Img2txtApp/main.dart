2547b82abeb1c78dbc142259806b4f99333db3da
EUNWOOPARK <pwu66@naver.com>
2021-07-15 22:25:52 +0900

    str2num
    i replaced the letters that look like numbers with numbers.

--- App/Img2txtApp/main.dart
+++ App/Img2txtApp/main.dart
@@ -16,15 +16,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -34,16 +25,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -64,25 +45,18 @@ class _MyHomePageState extends State<MyHomePage> {
     _controller = TextEditingController();
   }
 
-  // @override
-  // void dispose(){
-  //   _controller.dispose();
-  //   textDetector.dispose();
-  //   super.dispose();
-  // }
+  @override
+  void dispose(){
+    _controller!.dispose();
+    textDetector.close();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: _body(),
@@ -95,7 +69,6 @@ class _MyHomePageState extends State<MyHomePage> {
       padding: const EdgeInsets.all(8.0),
       child: Column(
         children: [
-          // SizedBox(height: 16,),
           BuildImage(),
           SizedBox(
             height: 8,
@@ -133,7 +106,6 @@ class _MyHomePageState extends State<MyHomePage> {
       ? Flexible(
           child: ListView.separated(
             padding: EdgeInsets.all(8.0),
-            //reverse: true,
             itemCount: message.length,
             itemBuilder: (_, int index) => _ListTile(index),
             separatorBuilder: (BuildContext context, int index) =>
@@ -142,18 +114,12 @@ class _MyHomePageState extends State<MyHomePage> {
         )
       : Container();
 
-  // Widget _ListTile(int index) {
-  //   return TextField(onSubmitted:(String value)=>{print(value)} ,);
-  //   }
-
   Widget _ListTile(int index) {
     return Row(
       children: [
         Flexible(
           child: ListTile(
             title: Center(child: Text(message[index])),
-            //selected: true,
-            // onTap: () {},
           ),
         ),
         //Icons.border_color
@@ -163,11 +129,11 @@ class _MyHomePageState extends State<MyHomePage> {
             },
             icon: Icon(Icons.add_rounded)),
 
-        IconButton(
-            onPressed: () {
-              _controller!.value = TextEditingValue(text: message[index]);
-            },
-            icon: Icon(Icons.border_color)),
+        // IconButton(
+        //     onPressed: () {
+        //       _controller!.value = TextEditingValue(text: message[index]);
+        //     },
+        //     icon: Icon(Icons.border_color)),
 
         IconButton(
             color: Colors.blue,
@@ -191,6 +157,14 @@ class _MyHomePageState extends State<MyHomePage> {
                   child: TextField(
                 controller: _controller,
               )),
+              IconButton(
+                  onPressed: () {
+                    _controller!.value = TextEditingValue(text: '');
+                  },
+                  icon: Icon(
+                    Icons.delete,
+                    color: Colors.black,
+                  )),
               IconButton(
                   onPressed: () {
                     FlutterClipboard.copy(_controller!.text);
@@ -252,7 +226,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Future _getImage(var what) async {
     Navigator.of(context).pop();
     final pickedFile = await ImagePicker.platform.pickImage(source: what);
-    //final pickedFile = await _imagePicker?.getImage(source: ImageSource.camera);
     if (pickedFile != null) {
       _processPickedFile(pickedFile);
     } else {
@@ -267,15 +240,43 @@ class _MyHomePageState extends State<MyHomePage> {
     });
     final inputImage = InputImage.fromFilePath(pickedFile.path);
     processImage(inputImage);
-    // final recognisedText = await textDetector.processImage(inputImage);
   }
-
+  String? str;
   Future<void> processImage(InputImage inputImage) async {
     message.clear();
     final recognisedText = await textDetector.processImage(inputImage);
     for (final textBlock in recognisedText.blocks) {
       for (final textLine in textBlock.lines) {
-        message.add(textLine.text);
+        var tmp = textLine.text;
+        if(textLine.text.contains('o')||textLine.text.contains('O')||textLine.text.contains('e')){
+          tmp = tmp.replaceAll(RegExp('o'), '0');
+          tmp = tmp.replaceAll(RegExp('O'), '0');
+          tmp = tmp.replaceAll(RegExp('e'), '0');
+        }
+        if(textLine.text.contains('l')||textLine.text.contains('I')||textLine.text.contains('/')||textLine.text.contains('\\|')) {
+          tmp = tmp.replaceAll(RegExp('l'), '1');
+          tmp = tmp.replaceAll(RegExp('I'), '1');
+          tmp = tmp.replaceAll(RegExp('/'), '1');
+          tmp = tmp.replaceAll(RegExp('\\|'), '1');
+        }
+        if(textLine.text.contains('S')||textLine.text.contains('s')) {
+          tmp = tmp.replaceAll(RegExp('S'), '5');
+          tmp = tmp.replaceAll(RegExp('s'), '5');
+        }
+        if(textLine.text.contains('b')) {
+          tmp = tmp.replaceAll(RegExp('b'), '6');
+        }
+
+        if(textLine.text.contains('M')||textLine.text.contains('m')||textLine.text.contains('n')||textLine.text.contains(')')) {
+          tmp = tmp.replaceAll(RegExp('M'), '7');
+          tmp = tmp.replaceAll(RegExp('m'), '7');
+          tmp = tmp.replaceAll(RegExp('n'), '7');
+          tmp = tmp.replaceAll(RegExp('\\)'), '7');
+        }
+        if(textLine.text.contains('q')) {
+          tmp = tmp.replaceAll(RegExp('q'), '9');
+        }
+        message.add(tmp);
       }
     }
     setState(() {});
